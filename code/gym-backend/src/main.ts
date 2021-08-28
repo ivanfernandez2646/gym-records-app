1af28bce9b651cc3c0e946a43fef68d5a805e5c8
Ivan Fernandez <ivanfernandez422000@gmail.com>
2021-08-28 19:58:48 +0200

    feat: Add CORS in BE

--- code/gym-backend/src/main.ts
+++ code/gym-backend/src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ credentials: true, origin: 'http://localhost:4200' });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
