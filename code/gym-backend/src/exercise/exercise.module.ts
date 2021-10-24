29dc2228233aae683bfa7cf0891c38c0c3a9c45f
Ivan Fernandez <ivanfernandez422000@gmail.com>
2021-10-24 11:55:00 +0200

    [BE] Run lint

--- code/gym-backend/src/exercise/exercise.module.ts
+++ code/gym-backend/src/exercise/exercise.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Exercise, ExerciseSchema } from 'src/schemas/exercise.schema';
-import { Mark, MarkSchema } from 'src/schemas/mark.schema';
 import { ExerciseController } from './exercise.controller';
 import { ExerciseService } from './exercise.service';
 
